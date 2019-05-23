7cbc4dcab7b3002ac9818b809f8d14a51616a00c
Will.I.AM <ratz@web.de>
2019-05-23 11:38:05 +0200

    threading working i think :+1:

---
+++ Source/MyStateMachPro/Lobby/NetworkSystem.cpp
@@ -0,0 +1,58 @@
+#include "NetworkSystem.h"
+
+NetworkSystem* NetworkSystem::NetSys = NULL;
+
+bool NetworkSystem::StartingMessageReceiveThread()
+{
+	MessageReceiveThread.InitThread(&socketUDP, m_receiveArray);
+
+	return false;
+}
+
+NetworkSystem::NetworkSystem()
+{
+
+}
+
+
+NetworkSystem::~NetworkSystem()
+{
+}
+
+void NetworkSystem::TaskMessageReceiveThread(char* receivearray)
+{
+}
+
+bool NetworkSystem::InitNetSystem()
+{
+	this->serverAddress = NetAddress(127, 0, 0, 1, 4405);
+
+	BWNet::InitializeSocketLayer();
+
+	if (socketUDP.OpenSocket(0).m_errorCode == 0)
+	{
+		if (socketUDP.EnableNonBlocking().m_errorCode == 0)
+		{
+			this->StartingMessageReceiveThread();
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void NetworkSystem::ClearReceiveArray()
+{
+	int a = sizeof(sendArray) / sizeof(*sendArray);
+	for (int i = 0; i < a; ++i)
+	{
+		m_receiveArray[i] = 0;
+		sendArray[i] = 0;
+	}
+}
--- Source/MyStateMachPro/Lobby/ResendMessageThread.h
+++ Source/MyStateMachPro/Lobby/ResendMessageThread.h
@@ -8,27 +8,26 @@
 #include "Runnable.h"
 #include "RunnableThread.h"
 
-class MYSTATEMACHPRO_API FMessageReceiveThread : public FRunnable
+class MYSTATEMACHPRO_API FResendMessageThread : public FRunnable
 {
 	NetSocketUDP* m_clientSocket;
 	char* m_receiveArray;
 
 public:
-	static  FMessageReceiveThread* Runnable;
+	static  FResendMessageThread* Runnable;
 
 	bool threadRuning = true;
 
 	FRunnableThread* Thread;
 
-	FMessageReceiveThread();
-	virtual ~FMessageReceiveThread();
+	FResendMessageThread();
+	virtual ~FResendMessageThread();
 
 	virtual bool Init();
 	virtual uint32 Run();
 
 	void EnsureCompletion();
 
-	void InitThread(NetSocketUDP* clientSocket, char* m_receiveArray);
 	static void Shutdown();
 
 	static bool IsThreadFinished();
