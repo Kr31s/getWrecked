9f5a1074f94ca0b78e814e1035a1e6f0a0b31c4d
Will.I.AM <ratz@web.de>
2019-08-06 16:01:01 +0200

    sdfgsdf

--- Source/MyStateMachPro/Lobby/NetworkSystem.cpp
+++ Source/MyStateMachPro/Lobby/NetworkSystem.cpp
@@ -418,18 +418,18 @@ void NetworkSystem::OppentGameMessage(char* p_receiveArray)
 				NetworkSystem::NetSys->gameMessagesRivale.insert(NetworkSystem::NetSys->gameMessagesRivale.begin(), GameMessageData(timeVal, inputVal));
 			}
 			NetworkSystem::NetSys->gameMessagesRivale.resize(249, GameMessageData());
-			for (int i = 0; i < 248; ++i)
-			{
-				NetworkSystem::NetSys->gameMessagesRivale[i].m_time;
-				NetworkSystem::NetSys->gameMessagesRivale[i + 1].m_time;
+			//for (int i = 0; i < 248; ++i)
+			//{
+			//	NetworkSystem::NetSys->gameMessagesRivale[i].m_time;
+			//	NetworkSystem::NetSys->gameMessagesRivale[i + 1].m_time;
 
-				if (NetworkSystem::NetSys->gameMessagesRivale[i].m_time - 1 == NetworkSystem::NetSys->gameMessagesRivale[i + 1].m_time) {
-					//GEngine->AddOnScreenDebugMessage(-1, 2.0F, FColor::Green, TEXT("Jop"));
-				}
-				else {
-					//GEngine->AddOnScreenDebugMessage(-1, 2.0F, FColor::Red, TEXT("Nop"));
-				}
-			}
+			//	if (NetworkSystem::NetSys->gameMessagesRivale[i].m_time - 1 == NetworkSystem::NetSys->gameMessagesRivale[i + 1].m_time) {
+			//		//GEngine->AddOnScreenDebugMessage(-1, 2.0F, FColor::Green, TEXT("Jop"));
+			//	}
+			//	else {
+			//		timeVal += 10;
+			//	}
+			//}
 		}
 	}
 
