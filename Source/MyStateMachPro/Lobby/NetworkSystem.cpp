7fdb548397cbfbebedae174434982b76a28dc6ab
WilliamRatz <ratz@web.de>
2019-08-06 21:25:27 +0200

    ticking

--- Source/MyStateMachPro/Lobby/NetworkSystem.cpp
+++ Source/MyStateMachPro/Lobby/NetworkSystem.cpp
@@ -338,6 +338,8 @@ void NetworkSystem::RoomJoin(char* p_receiveArray)
 		m_opponentName[i] = p_receiveArray[i + 2];
 	}
 	AMyStateMachProGameModeBase::m_opponentName = FString(UTF8_TO_TCHAR(m_opponentName));
+	m_gameMode->OnName2Changed.Broadcast(AMyStateMachProGameModeBase::m_opponentName);
+
 	if (UMyUserWidget::myUserWidget) {
 	UMyUserWidget::myUserWidget->RivalJoinMessage(FString(UTF8_TO_TCHAR(m_opponentName)));
 	}
@@ -391,6 +393,11 @@ void NetworkSystem::OppentGameMessage(char* p_receiveArray)
 	if (NetworkSystem::NetSys->gameMessagesRivale.size() == 0) {
 		NetworkSystem::NetSys->gameMessagesRivale.resize(249, GameMessageData());
 	}
+
+	if (!NetworkSystem::ticking) {
+		return;
+	}
+
 	unsigned short inputVal;
 	unsigned short timeVal;
 
